437684202e984ec1367a76abe85169bc80c6f508
Leyton Manuel Espitia Diaz <leyton_espitia@hotmail.com>
2016-12-29 16:42:07 -0500

    Menu del sitio web

---
+++ js/menu.js
@@ -0,0 +1,26 @@
+$(document).ready(main);
+var contador = 1;
+var ancho = $(document).width();
+function main (){
+	$('.menu_bar').click(function(){
+		// $('nav').toggle(); Forma Sencilla de aparecer y desaparecer
+		
+		if (contador == 1){
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		};
+		
+	});
+
+	// Mostramos y ocultamos submenus
+	$('.submenu').click(function(){
+		$(this).children('.children').slideToggle();
+	});
+};
