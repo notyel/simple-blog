face18dfa2729b049819f7cbef7b281f578273d9
Leyton M. Espitia Diaz <leyton_espitia@hotmail.com>
2025-05-24 16:31:40 -0500

    chore: remove all application files and configurations for blog-app

--- src/app/shared/state-keys.ts
+++
@@ -1,5 +0,0 @@
-import { makeStateKey } from '@angular/core';
-import { Post } from '../core/models/post.model';
-
-export const POSTS_KEY = makeStateKey<Post[]>('posts');
-export const POST_KEY = makeStateKey<Post>('post');
