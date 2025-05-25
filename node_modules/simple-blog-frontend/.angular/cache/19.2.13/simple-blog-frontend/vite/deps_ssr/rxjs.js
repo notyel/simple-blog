a81dbeb38bdfd1cb5ffdfc3c2621ed31dcd859a6
Leyton M. Espitia Diaz <leyton_espitia@hotmail.com>
2025-05-24 23:32:42 -0500

    chore: limpieza de archivos innecesarios y actualización de .gitignore

--- node_modules/simple-blog-frontend/.angular/cache/19.2.13/simple-blog-frontend/vite/deps_ssr/rxjs.js
+++
@@ -1,6 +0,0 @@
-import { createRequire } from 'module';const require = createRequire(import.meta.url);
-import {
-  require_cjs
-} from "./chunk-QYGMPYRZ.js";
-import "./chunk-YHCV7DAQ.js";
-export default require_cjs();
